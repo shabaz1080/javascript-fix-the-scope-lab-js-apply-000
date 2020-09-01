5e2164d3e76a1d92ab5b2ffb485c16cc39112d7b
whimsical-glitch-7555 <shabaz8010@gmail.com>
2020-09-01 04:42:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,9 +24,7 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  var theFunk = funkyFunction
   return function() {
-    
     return "FUNKY!"
   }
 }
@@ -34,3 +32,4 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+theFunk = 'FUNKY'
